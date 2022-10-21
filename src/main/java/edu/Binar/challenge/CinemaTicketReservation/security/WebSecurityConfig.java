d06c59f46189bb51ccce2d00ebd582bd2d931047
mfyasykur <mf.yasykur@gmail.com>
2022-10-21 22:27:37 +0700

    Update CORS  config

--- src/main/java/edu/Binar/challenge/CinemaTicketReservation/security/WebSecurityConfig.java
+++ src/main/java/edu/Binar/challenge/CinemaTicketReservation/security/WebSecurityConfig.java
@@ -23,24 +23,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new CorsFilter();
     }
 
-//    @Bean
-//    @Override
-//    public UserDetailsService userDetailsService() {
-//        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-//        userDetailsManager.createUser(User.withDefaultPasswordEncoder()
-//                .username("admin")
-//                .password("admin123")
-//                .roles("ADMIN")
-//                .build());
-//
-//        userDetailsManager.createUser(User.withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("user123")
-//                .roles("USER")
-//                .build());
-//
-//        return userDetailsManager;
-//    }
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        userDetailsManager.createUser(User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin123")
+                .roles("ADMIN")
+                .build());
+
+        userDetailsManager.createUser(User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user123")
+                .roles("USER")
+                .build());
+
+        return userDetailsManager;
+    }
 
 //    @Override
 //    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
