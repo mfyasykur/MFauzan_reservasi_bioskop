a19a5eb52fbe2fb41978de9afdb65956f36c525f
mfyasykur <mf.yasykur@gmail.com>
2022-10-21 21:32:01 +0700

    Update CORS config

--- src/main/java/edu/Binar/challenge/CinemaTicketReservation/security/WebSecurityConfig.java
+++ src/main/java/edu/Binar/challenge/CinemaTicketReservation/security/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.session.SessionManagementFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -66,6 +67,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout().permitAll();
 
-//        httpSecurity.cors();
+        httpSecurity.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
     }
 }
