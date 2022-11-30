c48c03759682b6adb9b744fe2a5ab7668a01e7a8
mfyasykur <mf.yasykur@gmail.com>
2022-11-30 11:50:17 +0700

    Update Firebase dependency

--- src/main/java/edu/Binar/challenge/CinemaTicketReservation/firebase/FireBaseInitialize.java
+++ src/main/java/edu/Binar/challenge/CinemaTicketReservation/firebase/FireBaseInitialize.java
@@ -3,6 +3,8 @@ package edu.Binar.challenge.CinemaTicketReservation.service;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -11,19 +13,21 @@ import java.io.FileInputStream;
 @Service
 public class FireBaseInitialize {
 
+    Logger logger = LoggerFactory.getLogger(FireBaseInitialize.class);
+
     @PostConstruct
     public void initialize() {
 
-        try (FileInputStream serviceAccount = new FileInputStream("./demofirebase-a8207-firebase-adminsdk-abmeh-a829142150.json")) {
+        try (FileInputStream serviceAccount = new FileInputStream("src/main/resources/MyCinema-ServiceAccount.json")) {
 
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setDatabaseUrl("https://demofirebase-a8207-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                    .setDatabaseUrl("https://mycinema-f7acc-default-rtdb.asia-southeast1.firebasedatabase.app/")
                     .build();
-
             FirebaseApp.initializeApp(options);
+            logger.info("Firebase application has been initialized");
         } catch (Exception exception) {
-            exception.printStackTrace();
+            logger.error(exception.getMessage());
         }
     }
 }
